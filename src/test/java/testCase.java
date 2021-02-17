91dcde6ec900d5bfb74330bd186f605d67ea6d66
468165108@qq.com <gp123123123>
2021-02-17 17:55:43 +0800

    题目1

--- src/test/java/testCase.java
+++ src/test/java/testCase.java
@@ -3,8 +3,43 @@ import org.junit.Test;
 public class testCase {
 
     @Test
-    public  void  case01(){
-        System.out.println("1212");
+    public void case01() {
+        int i = reverse(12345);
+        System.out.println(i);
     }
 
+
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int tmp = x % 10;
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && tmp > 7)) {
+                return 0;
+            }
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && tmp < -8)) {
+                return 0;
+            }
+
+            x = x / 10;
+            result = result * 10 + tmp;
+        }
+        return result;
+    }
+
+    public int reverse02(int x) {
+        int result = 0;
+        while (x != 0) {
+            int tmp = x % 10;
+            x = x / 10;
+
+            try {
+                result = result * 10 + tmp;
+            } catch (Exception e) {
+                return 0;
+            }
+        }
+        return result;
+
+    }
 }
+
